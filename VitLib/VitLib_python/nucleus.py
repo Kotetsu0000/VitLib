c315a534bcf9910545c2b11dcb07cfa12b52fe30
Kotetsu0000 <yuuiti1234@gmail.com>
2024-04-26 23:15:47 +0900

    nucleusモジュールにcalc_standard_nuclear_area関数を追加

--- VitLib/VitLib_python/nucleus.py
+++ VitLib/VitLib_python/nucleus.py
@@ -1,1 +1,37 @@
+import warnings
+
+import cv2
+import numpy as np
+
 from .common import smallAreaReduction
+
+def calc_standard_nuclear_area(ans_img:np.ndarray, lower_ratio:float=0, heigher_ratio:float=0):
+    """
+    標準的核面積を計算する
+
+    Args:
+        ans_img (np.ndarray): 二値化画像
+        lower_ratio (float): 除外する面積の下位割合(%) (0-100の範囲)
+        heigher_ratio (float): 除外する面積の上位割合(%) (0-100の範囲)
+
+    Returns:
+        float: 標準的核面積
+    
+    Note:
+        例としてlower_ratio=0.1, heigher_ratio=0.1の場合、下位10%と上位10%の面積を除外した中間の80%の面積を使用して標準的核面積の計算を行う
+    """
+    if lower_ratio + heigher_ratio < 0 or lower_ratio + heigher_ratio > 100:
+        raise ValueError("lower_ratio + heigher_ratio must be in the range of 0-100")
+
+    ans_unique_len = len(np.unique(ans_img))
+    if ans_unique_len != 2 and ans_unique_len != 1:
+        warnings.warn("ans_imgは二値画像ではありません。閾値127で二値化を行います。", UserWarning)
+        ans_img = cv2.threshold(ans_img, 127, 255, cv2.THRESH_BINARY)[1]
+
+    contours = cv2.findContours(ans_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    print(type(contours))
+    area_size = [cv2.contourArea(contour) for contour in contours]
+    out_lower_num = int(len(area_size)*lower_ratio/100)
+    out_heigher_num = int(len(area_size)*heigher_ratio/100)
+    sorted_area_size = sorted(area_size)[out_lower_num:-out_heigher_num]
+    return np.mean(sorted_area_size)
