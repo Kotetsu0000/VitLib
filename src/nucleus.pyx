3a9c6d1141d1975a1fc59d70d7b1ea14af7d6923
Kotetsu0000 <yuuiti1234@gmail.com>
2024-04-27 23:26:58 +0900

    VitLibのバージョンを2.0.23に更新しました

--- src/nucleus.pyx
+++ src/nucleus.pyx
@@ -89,13 +89,12 @@ cpdef dict make_eval_images(cnp.ndarray[DTYPE_t, ndim=2] ans_img, cnp.ndarray[DT
             - "red_img": DontCare領域画像
             - "green_img": 正解領域画像
     """
-    cdef cnp.ndarray[cnp.float32_t, ndim=1] area_size = calc_contour_areas(ans_img)
     cdef tuple contours = cv2.findContours(ans_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
     cdef float standard_nuclear_area = calc_standard_nuclear_area(ans_img, lower_ratio, heigher_ratio)
-    cdef list red, green
-    cdef int red_len, green_len
+    #cdef list red, green
+    #cdef int red_len, green_len
     #cdef cnp.ndarray[DTYPE_t, ndim=2] red_img, green_img
-    cdef cnp.ndarray[DTYPE_t, ndim=3] eval_img
+    #cdef cnp.ndarray[DTYPE_t, ndim=3] eval_img
     #cdef cnp.ndarray[DTYPE_t, ndim=2] kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     cdef dict return_dict = {}
 
