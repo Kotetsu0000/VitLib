a7f18ba9baf8c525d09ead4058363266a858d609
Kotetsu0000 <yuuiti1234@gmail.com>
2024-06-24 16:39:58 +0900

    sphinx upload

---
+++ docs/script/conf.py
@@ -0,0 +1,41 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+import sphinx_rtd_theme
+import sphinx_fontawesome
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'VitLib'
+copyright = '2024, Kotetsu0000'
+author = 'Kotetsu0000'
+release = '2.3.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.napoleon', 
+    'sphinx_fontawesome', 
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_default_flags = ['members', 'undoc-members', 'private-members']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_show_sourcelink = False
